f2b02a872c966ddc0edaab7e5edaa6090754df09
boksul <boksul@hotmail.com>
2019-02-13 01:43:24 +0000

    .gitignore added

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,29 @@
 var request = require('request');
-
+var secrets = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    },
+    authorization: secrets['GITHUB_TOKEN']
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function(err, resultString) {
+  // console.log("Errors:", err);
+  const results = JSON.parse(resultString);
+  for (var i = 0; i < results.length; i++) {
+    console.log(results[i].avatar_url)
+  }
+
+  // console.log('There are', results.length, 'results');
+  // console.log(JSON.stringify(JSON.parse(result), null, 2));
 });
+
